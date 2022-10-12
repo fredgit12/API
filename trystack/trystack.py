a1b90c99c1f74ed3922f4d97ecf01b2548e3b647
fred <a_fredi@yahoo.com>
2022-10-13 01:52:00 +0200

    adding first config from config class

--- trystack/trystack.py
+++ trystack/trystack.py
@@ -1,6 +1,9 @@
 import re
 from flask import Flask
+from .config import Config
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+    print(app.config)
     return app
