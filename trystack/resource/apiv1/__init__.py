0399ec39399b60564d0242e5a101f0cff1133a49
fred <a_fredi@yahoo.com>
2022-10-13 22:14:17 +0200

    add resource section

---
+++ trystack/resource/apiv1/__init__.py
@@ -0,0 +1,9 @@
+from crypt import methods
+from trystack.trystack import apiv1 as api
+from project import ProjectResource
+api.add_resource(
+    ProjectResource,
+    "/projects",
+    methods = [ "GET", "POST"],
+    endpoint = "projects"
+)
---
+++ trystack/resource/apiv1/project.py
@@ -0,0 +1,6 @@
+from operator import imod
+from flask_restful import Resource
+class ProjectResource(Resource):
+    def get(self):
+        return {}
+        
